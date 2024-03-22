98de96c9f9f66813aa787a4ca2d1f83b88829cec
Radu Dincă <dinca.radu@gmail.com>
2024-03-22 22:20:28 +0200

    Update KeepAlivePayload interface with IP and Hostname

--- src/modules/IoT-communication/iot-communication.interface.ts
+++ src/modules/IoT-communication/iot-communication.interface.ts
@@ -1,12 +1,9 @@
 export enum EventName {
-  MESSAGE = 'message',
   KeepAlive = 'keep_alive',
-}
-
-export interface MessagePayload {
-  message: string;
 }
 
 export interface KeepAlivePayload {
   now: number;
+  ip: string;
+  hostname: string;
 }
