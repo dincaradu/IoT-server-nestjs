11d611e06a12ea98e2e2101882efb44d7e405665
Radu Dincă <dinca.radu@gmail.com>
2024-03-22 22:17:05 +0200

    Add Humidity and Temperature modules to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { IoTCommunicationModule } from './modules/IoT-communication/iot-communication.module';
+import { HumidityModule } from './modules/humidity/humidity.module';
+import { TemperatureModule } from './modules/temperature/temperature.module';
 
 @Module({
-  imports: [IoTCommunicationModule],
+  imports: [IoTCommunicationModule, HumidityModule, TemperatureModule],
   controllers: [],
   providers: [],
 })
